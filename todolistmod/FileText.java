30b1c42cfcdc537c413cc81b7da46b4197c84be3
BOKD-286 <BOKD-286@proton.me>
2022-06-06 21:51:51 +0000

    USB: cp210x: add Virtenio Preon32 device id

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -9,4 +9,6 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
-import java.io.IOExcept
+import java.io.IOException;
+import java.util.logging.Level;
+import java.u
