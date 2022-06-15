0552e3a190b25d15a694806d22e7cad3400792e9
BOKD-286 <BOKD-286@proton.me>
2022-06-15 20:02:36 +0000

    Update copy.

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -50,4 +50,5 @@ public class FileText extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
-  
+                formWindowOpened(evt);
+ 
