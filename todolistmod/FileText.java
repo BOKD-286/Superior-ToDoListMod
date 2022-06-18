99c8219bdbdc8d8465a09e8ec8dd060aa676883a
BOKD-286 <BOKD-286@proton.me>
2022-06-18 08:37:12 +0000

    usb: wusbcore: set the RPIPE wMaxPacketSize value correctly

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -57,3 +57,4 @@ public class FileText extends javax.swing.JFrame {
         TextFileDisplay.setEditable(false);
         TextFileDisplay.setBackground(new java.awt.Color(204, 204, 204));
         TextFileDisplay.setColumns(20);
+        TextFileDisplay.setFont(new j
