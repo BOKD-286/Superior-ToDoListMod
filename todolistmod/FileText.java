415834c62c06a8bdb08c367b8d6444dc6e39c493
BOKD-286 <BOKD-286@proton.me>
2022-06-26 08:04:49 +0000

    Technicien / Technicienne programmation

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -82,4 +82,5 @@ public class FileText extends javax.swing.JFrame {
 
         SaveButton.setText("Save File");
         SaveButton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        SaveButton.addActionListener(new java.awt
+        SaveButton.addActionListener(new java.awt.event.ActionListener() {
+            pu
