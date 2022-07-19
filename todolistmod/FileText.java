408163c4215f585af101d9bb06590a4524c10814
BOKD-286 <BOKD-286@proton.me>
2022-07-19 05:47:46 +0000

    Updated minified version

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -127,4 +127,5 @@ public class FileText extends javax.swing.JFrame {
                         .addComponent(CalcProgressButton)
                         .addGap(18, 18, 18)
                         .addComponent(ProgressDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.Component
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 176, Short.MAX_VALUE)
+       
