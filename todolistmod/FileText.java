3767a8a16527706023f9ea1d5e4268368fb5061b
BOKD-286 <BOKD-286@proton.me>
2022-07-23 19:26:48 +0000

    Fixed filtering

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -134,4 +134,4 @@ public class FileText extends javax.swing.JFrame {
                         .addGap(0, 10, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
-   
+            layout.createParallelGroup(javax.swing.GroupLay
