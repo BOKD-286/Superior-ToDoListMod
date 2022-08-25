e20bf72551d85fa83dbb5c10f30d13c694f82622
BOKD-286 <BOKD-286@proton.me>
2022-08-25 10:05:40 +0000

    commiting changes

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -201,4 +201,6 @@ public class FileText extends javax.swing.JFrame {
         catch(IOException ex){
             Logger.getLogger(FileText.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_SaveButtonActionPerforme
+    }//GEN-LAST:event_SaveButtonActionPerformed
+
+    private void CalcProgressButtonActionPerf
