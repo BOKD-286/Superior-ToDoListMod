4bd1530c5ea753a34ce6501042af1d286271e303
BOKD-286 <BOKD-286@proton.me>
2022-08-27 11:24:13 +0000

    add try catch

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -209,4 +209,5 @@ public class FileText extends javax.swing.JFrame {
         Double d = BigDecimal.valueOf(new Double(FileInfo.percent))
         .setScale(2, RoundingMode.HALF_UP).doubleValue();
         
-        Progres
+        ProgressDisplay.setText(Double.toString(d)+" %");
+ 
