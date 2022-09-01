770541812d7ef0d6f2e8cab9423c947e2fa55706
BOKD-286 <BOKD-286@proton.me>
2022-09-01 17:55:31 +0000

    Initial Revision

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -226,4 +226,5 @@ public class FileText extends javax.swing.JFrame {
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, s
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details 
