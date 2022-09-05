339321e3a921f33f20eb1ad382c54796a0c8dc08
BOKD-286 <BOKD-286@proton.me>
2022-09-05 11:14:03 +0000

    Added new javadoc.

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -232,4 +232,4 @@ public class FileText extends javax.swing.JFrame {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                 
+                    javax.swing.UIManager.setLookAndFeel
