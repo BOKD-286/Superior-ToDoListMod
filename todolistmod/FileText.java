a57096d5e9f37559f64957772acd076bd03ea9f0
BOKD-286 <BOKD-286@proton.me>
2022-09-06 19:30:26 +0000

    conflict solved

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -232,4 +232,6 @@ public class FileText extends javax.swing.JFrame {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+       
