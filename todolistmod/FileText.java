657c0d90448153046eb7a1bed380045ea53f7b20
BOKD-286 <BOKD-286@proton.me>
2022-09-13 09:16:00 +0000

    added setup script

--- todolistmod/FileText.java
+++ todolistmod/FileText.java
@@ -243,4 +243,6 @@ public class FileText extends javax.swing.JFrame {
         } catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(FileText.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FileText.class.getName()).log(
+            java.util.logging.Logger.getLogger(FileText.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+   
